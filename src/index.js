7726aacf84eced70469ead9f4dcbfd6f0bc6a070
unknown <fnarayev@gmail.com>
2023-02-19 17:25:31 +0300

    feat: write solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let arr = n.toString().split('');
+    let newArr=[];
+    for (let i=arr.length-1; i>=0 ; i--) {
+        if (arr[i] == '-') 
+            continue
+        else 
+            newArr.push(arr[i]);
+    }
+    let res = +newArr.join('');
+    return res
 }
